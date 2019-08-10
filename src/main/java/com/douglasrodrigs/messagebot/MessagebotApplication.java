18d9f87b4b668277b318c4dfcb9c2bf5a5891f19
douglas.souza <douglas.souza@icolabora.com.br>
2019-08-10 17:26:54 -0300

    Iniciando

---
+++ src/main/java/com/douglasrodrigs/messagebot/MessagebotApplication.java
@@ -0,0 +1,13 @@
+package com.douglasrodrigs.messagebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagebotApplication.class, args);
+	}
+
+}
